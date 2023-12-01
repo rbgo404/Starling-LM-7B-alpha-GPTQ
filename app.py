7aa52f7e091b30bd6a7c3dfa5d145b122e446fa0
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-12-01 12:10:05 +0000

    template for Starling-LM-7B-alpha-GPTQ

---
+++ app.py
@@ -0,0 +1,29 @@
+import json
+import numpy as np
+import torch
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
+import base64
+from io import BytesIO
+
+class InferlessPythonModel:
+  def initialize(self):
+      self.tokenizer = AutoTokenizer.from_pretrained("TheBloke/Starling-LM-7B-alpha-GPTQ", use_fast=True)
+      self.model = AutoGPTQForCausalLM.from_quantized(
+        "TheBloke/Starling-LM-7B-alpha-GPTQ",
+        use_safetensors=True,
+        device="cuda:0",
+        quantize_config=None,
+        inject_fused_attention=False
+      )
+
+  def infer(self, inputs):
+    prompt = inputs["prompt"]
+    input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+    output = self.model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=200)
+    result = self.tokenizer.decode(output[0])
+    return {"generated_result": result}
+
+  def finalize(self,args):
+    self.tokenizer = None
+    self.model = None
